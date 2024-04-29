15ff29aeebdd1769a6ea0ae9be1a5e006fbf17a2
SwanLamirault <swan.lamirault@gmail.com>
2024-04-29 16:58:50 +0200

    Création des temples

--- src/main/java/modele/Temple.java
+++ src/main/java/modele/Temple.java
@@ -1,5 +1,21 @@
 package modele;
 
-public class Temple {
+import javafx.scene.paint.Color;
+import javafx.util.Pair;
 
+public class Temple {
+     Pair<Integer, Integer> position;
+     Color couleur;
+
+
+     //--------------------------Constructeur--------------------------
+     public Temple(Pair<Integer, Integer> parPosition, Color parCouleur){
+          position = parPosition;
+          couleur = parCouleur;
+     }
+
+
+     public String toString(){
+          return position + " avec la couleur " + couleur;
+     }
 }
