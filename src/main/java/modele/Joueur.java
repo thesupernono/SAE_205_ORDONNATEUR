745f7b6fd2a611d97e023e2676cefb6b8fd45bdd
thesupernono <noagauch.addrpro@gmail.com>
2024-04-29 17:03:29 +0200

    Prototypage de la classe Joueur

---
+++ src/main/java/modele/Joueur.java
@@ -0,0 +1,19 @@
+package modele;
+
+import javafx.util.Pair;
+
+public class Joueur {
+//    private String chPosX = "Position X";
+    //Cristal chCrisStocke;
+
+    public Joueur(){
+//        chPosX = 0;
+//        chPosY = 0;
+//
+//        private Pair
+
+    }
+
+    public int deplacement;
+
+}
