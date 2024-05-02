a6de06810f37d438f511372aa9a819e2ae592cae
SwanLamirault <swan.lamirault@gmail.com>
2024-05-02 15:12:08 +0200

    Correction de l'erreur de l'Application

--- src/main/java/vue/ApplicationJeu.java
+++ src/main/java/vue/ApplicationJeu.java
@@ -6,11 +6,13 @@ import javafx.stage.Stage;
 
 public class ApplicationJeu extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         VBoxTemple root = new VBoxTemple();
         Scene scene = new Scene(root, 1280,720);
         stage.setTitle("SAE ORDONNATEUR");
         stage.show();
+    }
+    public static void main(String [] args){
         Application.launch();
     }
 }
