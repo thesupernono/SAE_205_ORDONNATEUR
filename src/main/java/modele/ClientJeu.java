37a27b107b31f63e1c98f6f99089f0c50fe17b69
SwanLamirault <swan.lamirault@gmail.com>
2024-05-06 15:05:34 +0200

    Optimisation des créations de temples et de cristaux, vu que deux éléments ne peuvent pas être au même coordonnées

--- src/main/java/modele/ClientJeu.java
+++ src/main/java/modele/ClientJeu.java
@@ -2,15 +2,8 @@ package modele;
 
 public class ClientJeu {
     public static void main(String [] args) throws Exception {
-        // premier essai d'ajout d'un temple
-        int couleurTemple = 1;
 
-        Temple premierTemplate = new Temple(0, 0, couleurTemple);
-
-        LectureScenario lecture = new LectureScenario("Scenario1.txt");
-
-        Joueur sorcier = new Joueur(0,0);
-
+        Map map = new Map("Scenario1.txt");
 
     }
 }
--- src/main/java/modele/Map.java
+++ src/main/java/modele/Map.java
@@ -5,8 +5,11 @@ import java.util.HashSet;
 
 public class Map implements CONSTANTES_MAP{
 
-    HashMap<int [], HashSet<Temple>> coordonneesTemples = new HashMap<>();
-    HashMap<int [], HashSet<Cristal>> coordonneesCristal = new HashMap<>();
+    // Les cristaux et les temples sont stockés avec comme clé leur position (position_x, position_y)
+    // Ils ont comme valeur un
+
+    HashMap<int [], Temple> coordonneesTemples = new HashMap<>();
+    HashMap<int [], Cristal> coordonneesCristaux = new HashMap<>();
 
     public Map(String parNomFichierScenario) throws Exception {
 
@@ -24,18 +27,8 @@ public class Map implements CONSTANTES_MAP{
         for(Temple temple: listeTemple){
 
 
-            // On vérifie si d'autres éléments ont déjà été initié à cet emplacement
-            // On initie si il n'y a pas encore d'élément
-            if (!coordonneesTemples.containsKey(temple.getPosition())){
-                HashSet<Temple> nouveauHashSet = new HashSet<>();
-                nouveauHashSet.add(temple);
-                coordonneesTemples.put(temple.getPosition(), nouveauHashSet);
-            }
-            else{
-                // Si il y a déjà un élément, on reprend juste la liste associé à la position, et on ajoute le temple
-                HashSet<Temple> listeAuCoordonnees = coordonneesTemples.get(temple.getPosition());
-                listeAuCoordonnees.add(temple);
-            }
+            // On initie le temple
+            coordonneesTemples.put(temple.getPosition(), temple);
         }
 
 
@@ -43,18 +36,18 @@ public class Map implements CONSTANTES_MAP{
         for(Cristal cristal: listeCristal){
 
 
-            // On vérifie si d'autres éléments ont déjà été initié à cet emplacement
-            // On initie si il n'y a pas encore d'élément
-            if (!coordonneesCristal.containsKey(cristal.getPosition())){
-                HashSet<Cristal> nouveauHashSet = new HashSet<>();
-                nouveauHashSet.add(cristal);
-                coordonneesCristal.put(cristal.getPosition(), nouveauHashSet);
-            }
-            else{
-                // Si il y a déjà un élément, on reprend juste la liste associé à la position, et on ajoute le temple
-                HashSet<Cristal> listeAuCoordonnees = coordonneesCristal.get(cristal.getPosition());
-                listeAuCoordonnees.add(cristal);
-            }
+            // On initie les cristaux
+            coordonneesCristaux.put(cristal.getPosition(), cristal);
         }
+    }
+
+
+    public HashMap<int [], Temple> getCoordonneesTemples(){
+        return coordonneesTemples;
+    }
+
+
+    public HashMap<int [], Cristal> getCoordonneesCristaux(){
+        return coordonneesCristaux;
     }
 }
