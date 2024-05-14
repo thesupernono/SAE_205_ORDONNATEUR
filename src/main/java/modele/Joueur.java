3e6c549549fbaa2fd6de9afb5e9457edae45d2e0
SwanLamirault <swan.lamirault@gmail.com>
2024-05-14 17:04:00 +0200

    Déplacement du joueur

--- src/main/java/modele/Joueur.java
+++ src/main/java/modele/Joueur.java
@@ -14,13 +14,44 @@ public class Joueur {
     public Joueur(int PosX, int PosY){
         position = new int [2];
 
+        position[0] = PosX;
+        position[1] = PosY;
+
 
         cristaux = null;
     }
 
-   public void deplacement(int posX, int posY){
+    /**
+     * Téléporteur pour vérifier si le joueur peut bien sé déplacer
+     * @param posX (int) : position à téléporter (en x)
+     * @param posY (int) : position à téléporter (en y)
+     */
+    public void teleporteur(int posX, int posY){
       position[0] = posX;
       position[1] = posY;
+    }
+
+
+    /**
+     * Déplace le joueur jusqu'à qu'il soit arrivé
+     * @param positionArrivee (int []) : position d'arrivée
+     */
+    public void deplacement(int [] positionArrivee){
+        while(position != positionArrivee){
+            if(position[0] != positionArrivee[0]){
+                if(position[0] < positionArrivee[0])
+                    position[0] ++; // On se décale à droite
+                else
+                    position[0]--; // On se décale à gauche
+            }
+            else if(position[1] != positionArrivee[1]){
+                if(position[1] < positionArrivee[1])
+                    position[1] ++; // On se décale en bas
+                else
+                    position[1]--; // On se décale en haut
+            }
+        }
+
   }
 
     /**
