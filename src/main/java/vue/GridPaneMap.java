87d3008c23cd8f3c8762b30e612befcc4b5546c5
thesupernono <noagauch.addrpro@gmail.com>
2024-05-14 17:16:09 +0200

    Creation d'un evenement pour toutes les Panes (WIP!)

--- src/main/java/vue/GridPaneMap.java
+++ src/main/java/vue/GridPaneMap.java
@@ -1,24 +1,34 @@
 package vue;
 
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class GridPaneMap extends GridPane {
     public GridPaneMap(){
         //rends les lignes de la GridPane visible
         this.setGridLinesVisible(true);
 
-        this.setPadding(new Insets(10));
-        this.setHgap(5);
-        this.setVgap(5);
+        this.setPadding(new Insets(20));
+        this.setHgap(20);
+        this.setVgap(20);
 
         int ligne = 0;
 
         for(int x=0; x<10; x++){
-            this.addColumn(x,new Label(" "));
+            this.addColumn(1);
             for(int y=0; y<10; y++){
-                this.addRow(y,new Label(" "));
+                Pane cellule = new Pane();
+//                cellule.setOnContextMenuRequested(e -> {
+//                    menuItem1.setOnAction(evt -> System.out.println("Item 1 selected in cell ["+col+", "+row+"]"));
+//                    menuItem2.setOnAction(evt -> System.out.println("Item 2 selected in cell ["+col+", "+row+"]"));
+//                });
+                cellule.setOnMouseClicked(evenementClique -> {
+                    System.out.println("je suis clickée ahhh");
+                });
+                this.add(cellule, x, y);
             }
         }
     }
