6840bd101e94ff91a209452238b96cdfdbf07bd6
thesupernono <noagauch.addrpro@gmail.com>
2024-05-16 14:06:20 +0200

    Implementation du sujet de SAE.

--- src/main/java/modele/CONSTANTES_MAP.java
+++ src/main/java/modele/CONSTANTES_MAP.java
@@ -5,10 +5,11 @@ import javafx.scene.paint.Color;
 public interface CONSTANTES_MAP {
 
     //-------------------------Map-------------------------
-    int [] TAILLE_MAP = {26, 26};
+    int [] TAILLE_MAP = {1000,500};
     int CENTRE_X = TAILLE_MAP[0]/2;
     int CENTRE_Y = TAILLE_MAP[1]/2;
-
+    Color COULEUR_GRILLE = Color.BLACK;
+    int CARRE = 50;
 
     //------------------------Joueur------------------------
     Color [] COULEURS= {
@@ -17,5 +18,7 @@ public interface CONSTANTES_MAP {
             Color.PINK
     };
     int [] POSITION_DEPART = {CENTRE_X, CENTRE_Y};
-
+    int LARGEUR_OVALE = 30;
+    int HAUTEUR_OVALE = 30;
+    Color COULEUR_DESTINATION = Color.RED;
 }
