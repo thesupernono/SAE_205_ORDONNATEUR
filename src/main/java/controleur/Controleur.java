6e9129b8a486bfb3c2157c1c19fc2dcef4a1be93
SwanLamirault <swan.lamirault@gmail.com>
2024-05-27 09:18:12 +0200

    Ajout des graphiques dans le controleur

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -6,13 +6,13 @@ import javafx.scene.control.MenuItem;
 import modele.Cristal;
 import modele.LectureScenario;
 import modele.Temple;
-
+import javafx.scene.canvas.GraphicsContext;
 import java.io.File;
-import java.lang.invoke.DelegatingMethodHandle$Holder;
 import java.util.HashSet;
 
 public class Controleur implements EventHandler {
-    public LectureScenario scenario;
+    LectureScenario scenario;
+    GraphicsContext graphiqueContext2D;
 
     @Override
     public void handle(Event event) {
@@ -20,16 +20,18 @@ public class Controleur implements EventHandler {
 
         if (userData instanceof File) {
             File fichierScenario = (File) userData;
-
             System.out.println(fichierScenario.getName());
-
-
             // Lecture du fichier
             try {
                 scenario = new LectureScenario(fichierScenario.getName());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
+
+            // assignation des temples et des cristaux
+            HashSet<Temple> temples = scenario.getTemple();
+            HashSet <Cristal> cristaux = scenario.getCristal();
+
         }
     }
 }
